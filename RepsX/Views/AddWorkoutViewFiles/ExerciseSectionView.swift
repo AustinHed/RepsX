7c079e6ffbc483c51ce3f9904adc79970593f522
Austin Hed <Austin.hed@outlook.com>
2025-02-24 16:24:04 -0600

    ExerciseSectionView
    - update reps field for a set to be an adjustable text field
    - updated the setNumber to be gray if there are 0 associated reps

--- RepsX/Views/AddWorkoutViewFiles/ExerciseSectionView.swift
+++ RepsX/Views/AddWorkoutViewFiles/ExerciseSectionView.swift
@@ -32,37 +32,37 @@ struct ExerciseSectionView: View {
         
         //sets
         ForEach(Array(exercise.sets.enumerated()), id: \.element.id) { index, set in
-            HStack {
+            HStack  {
                 //number
                 Text("\(index + 1)")
                     .font(.caption)
                     .bold()
                     .frame(width: 24, height: 24)
-                    .background(Circle().fill(Color.blue))
-                    .foregroundColor(.white)
+                    .background(
+                        Circle()
+                            .stroke(set.reps == 0 ? Color.gray : Color.blue,
+                                    lineWidth: 1.5
+                                   )
+                    )
+                    .padding(.trailing)
+                    .foregroundColor(set.reps == 0 ? Color.gray : Color.blue)
                 
-                //editable weight
+                //weight
                 setWeightField(for: set)
-                    .padding(.horizontal,8)
                 
                 //reps
-                VStack(alignment:.leading){
-                    Text("Reps")
-                        .font(.caption)
-                        .foregroundStyle(.secondary)
-                    Text(set.reps.description)
-                    //.bold()
-                }
-                .padding(.horizontal,8)
+                setRepsField(for: set)
                 
                 //notes
                 VStack(alignment:.leading){
                     Text("Notes")
                         .font(.caption)
                         .foregroundStyle(.secondary)
-                    Text(".")
+                    Text("Empty")
                 }
                 .padding(.horizontal,8)
+                .frame(maxHeight: 15)
+
             }
         }
         
@@ -76,6 +76,8 @@ struct ExerciseSectionView: View {
     
 }
 
+//MARK: Weight and Rep fields
+//weight
 extension ExerciseSectionView {
     /// Returns an editable weight field view for a given set.
     func setWeightField(for set: Set) -> some View {
@@ -92,14 +94,17 @@ extension ExerciseSectionView {
                 set: { newValue in
                     if newValue.isEmpty {
                         set.setWeight = 0
+                        print("update setWeight with an empty value, use 0")
                     } else if let number = Double(newValue) {
                         set.setWeight = number
+                        print("update setWeight with valid value")
                     } else {
                         set.setWeight = 0
-                        print("the setWeight value was invalid")
+                        print("update setWeight with invalid value, use 0")
                     }
                 }
             ))
+            .frame(maxWidth: 50, maxHeight: 15)
             .focused($isKeyboardActive)
             .keyboardType(.decimalPad)
             .submitLabel(.done)
@@ -107,7 +112,44 @@ extension ExerciseSectionView {
                 exerciseViewModel.updateWeight(set, newWeight: set.setWeight)
             }
         }
-        .padding(.horizontal, 8)
+        
+    }
+}
+//reps
+extension ExerciseSectionView{
+    func setRepsField(for set: Set) -> some View {
+        VStack(alignment: .leading) {
+            Text("Reps")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+            
+            TextField("0", text: Binding (
+                get: {
+                    let formatted = String(set.reps)
+                    return formatted == "0" ? "" : formatted
+                },
+                set: { newValue in
+                    if newValue.isEmpty {
+                        set.reps = 0
+                        print("update rep with an empty value, use 0")
+                    } else if let number = Int(newValue) {
+                        set.reps = number
+                        print("update rep with valid value")
+                    } else {
+                        set.reps = 0
+                        print("update reps with invalid value, use 0")
+                    }
+                }
+            ))
+            .frame(maxWidth: 50, maxHeight: 15)
+            .focused($isKeyboardActive)
+            .keyboardType(.numberPad)
+            .submitLabel(.done)
+            .onSubmit {
+                exerciseViewModel.updateReps(set, newReps: set.reps)
+            }
+        }
+        
     }
 }
 
