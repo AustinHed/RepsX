9cf99216a7a2845303e9de55f5d3389014d76d03
Austin Hed <Austin.hed@outlook.com>
2025-02-28 13:56:35 -0600

    Updated rating to be show on LogView, with a colorBar vs Stars

--- RepsX/Views/LogViewFiles/LogViewRow.swift
+++ RepsX/Views/LogViewFiles/LogViewRow.swift
@@ -15,6 +15,7 @@ struct LogViewRow: View {
         HStack(alignment: .top) {
             // Date block (day-of-week and day)
             dateBlock(for: workout)
+                .padding(.leading)
             
             // Workout details (name, duration, and exercise summary)
             workoutDetails(for: workout)
@@ -24,6 +25,17 @@ struct LogViewRow: View {
         .padding(.trailing, 20)
         .background(Color.white)
         .cornerRadius(12)
+        //ratings overlay color
+        .overlay(
+            UnevenRoundedRectangle(topLeadingRadius: 12, bottomLeadingRadius: 12)
+                .foregroundStyle(
+                    colorForRating(workout.rating)
+                    //Color.mint
+                )
+                .frame(width:12),
+            alignment: .leading
+        )
+        
     }
 }
 
@@ -77,5 +89,64 @@ extension LogViewRow{
                 }
             }
         }
+        
     }
+}
+
+//MARK: Color function
+extension LogViewRow {
+    // Helper function to map rating to color
+    func colorForRating(_ rating: Int?) -> Color {
+        guard let rating = rating else {
+            return Color.gray
+        }
+        
+        switch rating {
+        case 5:
+            return Color(hex: "3E7CB2")
+        case 4:
+            return Color(hex: "6BAC66")
+        case 3:
+            return Color(hex: "DFD764")
+        case 2:
+            return Color(hex: "F99246")
+        case 1:
+            return Color(hex: "BF4448")
+        default:
+            return Color.gray
+        }
+    }
+}
+
+// Hex initializer for Color
+extension Color {
+    init(hex: String) {
+        // Remove any non-alphanumeric characters (like '#' or spaces)
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r = (int >> 16) & 0xFF
+        let g = (int >> 8) & 0xFF
+        let b = int & 0xFF
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: 1.0
+        )
+    }
+}
+
+#Preview {
+    let testWorkout:Workout = Workout(id: UUID(), name: "Test Workout", startTime: Date(), endTime: nil, rating: 4, exercises: [])
+    List{
+        LogViewRow(workout: testWorkout)
+            .listRowBackground(Color.clear)
+            .listRowSeparator(Visibility.hidden)
+            .listRowInsets(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+    }
+    .contentMargins(.horizontal,0)
+    .navigationTitle("Log")
+    .modelContainer(SampleData.shared.modelContainer)
 }
